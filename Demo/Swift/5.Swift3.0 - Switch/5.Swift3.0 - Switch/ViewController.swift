4bea03af18f4bb24c6a251f0d49c97560c5d4cfb
WzhGoSky <871708991@qq.com>
2016-09-22 22:22:36 +0800

    Swift - switch

---
+++ Demo/Swift/5.Swift3.0 - Switch/5.Swift3.0 - Switch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  5.Swift3.0 - Switch
+//
+//  Created by Hayder on 16/9/22.
+//  Copyright © 2016年 wangzhenhai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    /**
+    1.OC中 Switch() 中的值必须是整数
+    2.每个语句都需要一个 break
+    3.
+     */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
+
