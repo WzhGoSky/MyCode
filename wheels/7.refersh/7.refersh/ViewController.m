2b5ea12e45a06a58e6b40cac9e173d2d36e1d604
WzhGoSky <871708991@qq.com>
2016-11-03 22:56:05 +0800

    wheels Refersh

--- wheels/7.refersh/7.refersh/ViewController.m
+++ wheels/7.refersh/7.refersh/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "ZHPullUpFooterView.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) NSMutableArray *dataList;
+
+@property (nonatomic, strong) ZHPullUpFooterView *footerView;
 
 @end
 
@@ -16,14 +21,53 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //注册cell
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
+    
+    self.tableView.tableFooterView = [[UIView alloc] init];
+    
+    [self.tableView addSubview: self.footerView];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark --------------------tableViewDelegate&&dataSource------------------------
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.dataList.count;
 }
 
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    
+    cell.textLabel.text = self.dataList[indexPath.row];
+    return cell;
+}
 
+#pragma mark ------------------------setter && getter------------------------
+- (NSMutableArray *)dataList
+{
+    if (!_dataList) {
+        
+        _dataList = [NSMutableArray array];
+        
+        for (int i=0; i< 15; i++) {
+            
+            [_dataList addObject:[NSString stringWithFormat:@"原始数据%d",i]];
+        }
+    }
+    
+    return _dataList;
+}
+
+- (ZHPullUpFooterView *)footerView
+{
+    if (!_footerView) {
+        
+        _footerView = [[ZHPullUpFooterView alloc] init];
+    }
+    
+    return _footerView;
+}
 @end
---
+++ wheels/7.refersh/7.refersh/ZHPullUpFooterView.h
@@ -0,0 +1,13 @@
+//
+//  ZHPullUpFooterView.h
+//  7.refersh
+//
+//  Created by Hayder on 2016/11/3.
+//  Copyright © 2016年 wangzhenhai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZHPullUpFooterView : UIView
+
+@end
